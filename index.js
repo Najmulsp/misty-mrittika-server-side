de3fad6db8f52454c461ac34c852848d80789377
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-04-27 00:42:02 +0600

    added my craft delete route

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ app.put('/updateCraft/:id', async(req, res) =>{
   res.send(result)
 })
 
+// delete craft from my craft list
+app.delete('/deleteMyCraft/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query ={_id: new ObjectId(id)};
+  const result = await craftCollection.deleteOne(query)
+  res.send(result)
+})
     
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
