9917f1733dc5864763c32b5e5a27d5787d82eab7
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-04-30 22:53:36 +0600

    finished project

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const craftCollection = client.db('craftdb').collection('craft');
 
     const subcategories = client.db('subcategorydb').collection('subcategries');
